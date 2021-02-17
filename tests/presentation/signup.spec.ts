9b40b5db56c059e74870346be5a6a6a2c9648d70
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-17 11:33:11 -0300

    test: ensure SignUpController return 400 if no password is provided

--- tests/presentation/signup.spec.ts
+++ tests/presentation/signup.spec.ts
@@ -34,4 +34,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 })
