05970a3671c87b39a2a1196d02af8d46eb200ea2
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-17 10:14:15 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ tests/presentation/signup.spec.ts
@@ -0,0 +1,21 @@
+import { SignUpController } from '../../src/presentation/controllers/signup'
+import { HttpRequest, HttpResponse } from '../../src/presentation/protocols/http'
+
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
