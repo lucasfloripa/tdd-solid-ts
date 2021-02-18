187af7bff351a0b993d3ec7c87ac93c7593e15a9
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-18 11:03:38 -0300

    test: ensure DbAddAccount calls Encrypter with correct password

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,36 @@
+import { Encrypter } from '../../protocols/encrypter'
+import { DbAddAccount } from './db-add-account'
+
+const makeEncrypterStub = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return { sut, encrypterStub }
+}
+
+describe('DbAddAccount Usecase', () => {
+  test('Should call Encrypter with correct password (integration test)', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    await sut.add(accountData)
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
+})
