bb09d7c605fb2f13b5a922cf3b6de5be4c180e81
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-18 14:41:55 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -78,4 +78,16 @@ describe('DbAddAccount Usecase', () => {
     const promise: Promise<AccountModel> = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const promise: Promise<AccountModel> = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
