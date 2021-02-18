859dd27cd54094fc0e9d8d0f59be7a32c51f1170
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-18 15:55:01 -0300

    feat: ensure BcryptAdapter calls bycrypt with correct values

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -2,7 +2,12 @@ import { Encrypter } from '../../data/protocols/encrypter'
 import bcrypt from 'bcrypt'
 
 export class BcrypterAdapter implements Encrypter {
+  constructor (
+    private readonly salt: number
+  ) {}
+
   async encrypt (password: string): Promise<string> {
+    await bcrypt.hash(password, this.salt)
     return ''
   }
 }
