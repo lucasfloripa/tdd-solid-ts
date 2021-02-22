76a17cdbf396e78432a89a0c51ce8939facbaa79
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-22 08:25:17 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,10 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): any => {
+  const sut = new AccountMongoRepository()
+  return { sut }
+}
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
@@ -9,7 +15,17 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return an account on succes', () => {
-    expect('1').toBeTruthy()
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(accountData).toBeTruthy()
+    expect(accountData.id).toBeTruthy()
+    expect(accountData.name).toBe('any_name')
+    expect(accountData.email).toBe('any_email@mail.com')
+    expect(accountData.password).toBe('any_password')
   })
 })
