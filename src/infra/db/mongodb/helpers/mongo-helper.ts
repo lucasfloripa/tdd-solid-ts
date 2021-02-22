cd51e04fcf4807dbc542c7b31961d03558f2c698
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-22 08:38:52 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -13,5 +13,8 @@ export const MongoHelper = {
   },
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+  map (collection: any): any {
+    return { ...collection, id: collection._id }
   }
 }
