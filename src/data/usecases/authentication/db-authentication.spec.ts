32352feab8989d4f09acc7bcd90acd1ff94e6d3f
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-26 15:39:03 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -54,4 +54,12 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(fakeAuthenticationModel)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should returns null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const fakeAuthenticationModel = makeFakeAuthenticationModel()
+    const accessToken = await sut.auth(fakeAuthenticationModel)
+    expect(accessToken).toBeNull()
+  })
 })
