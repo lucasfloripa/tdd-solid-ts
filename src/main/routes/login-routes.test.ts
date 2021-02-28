cf3bfc3323e85d129a320a830b30c227bfcf8c84
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-02-28 15:45:16 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -42,7 +42,6 @@ describe('Login Routes', () => {
         email: 'lucasg@mail.com',
         password
       })
-
       await request(app)
         .post('/api/login')
         .send({
@@ -50,6 +49,16 @@ describe('Login Routes', () => {
           password: '123'
         })
         .expect(200)
+    })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lucasg@mail.com',
+          password: '123'
+        })
+        .expect(401)
     })
   })
 })
