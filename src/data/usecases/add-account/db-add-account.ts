5b07f970ec0f54c1820671805a29f7e4e0d2f488
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-10 13:39:56 -0300

    feat: ensure DbAddAccount calls encrypter with correct value

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,20 @@
+import { Encrypter } from '../authentication/db-authentication-protocols'
 import { AccountModel, AddAccount, AddAccountModel, Hasher, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly tokenGenerator: Encrypter
   ) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassord = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassord })
-    return account
+    const accessToken = await this.tokenGenerator.encrypt(account.id)
+
+
+    return
+     account
   }
 }
