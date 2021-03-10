9cd8452a6f7d59deb1427d8c98a025ec5665bbed
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-10 13:57:41 -0300

    refactor: change tokenGenerator approuch

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -15,15 +15,6 @@ const makeFakeAccountData = (): AddAccountModel => ({
   password: 'valid_password'
 })
 
-const makeEncrypterStub = (): Encrypter => {
-  class EncrypterStub implements Encrypter {
-    async encrypt(value: string): Promise<string> {
-      return await new Promise(resolve => resolve('any_token'))
-    }
-  }
-  return new EncrypterStub()
-}
-
 const makeHasherStub = (): Hasher => {
   class HasherStub implements Hasher {
     async hash(value: string): Promise<string> {
@@ -46,15 +37,13 @@ interface SutTypes {
   sut: DbAddAccount
   hasherStub: Hasher
   addAccountRepositoryStub: AddAccountRepository
-  encrypterStub: Encrypter
 }
 
 const makeSut = (): SutTypes => {
   const hasherStub = makeHasherStub()
   const addAccountRepositoryStub = makeAddAccountRepositoryStub()
-  const encrypterStub = makeEncrypterStub()
-  const sut = new DbAddAccount(hasherStub, addAccountRepositoryStub, encrypterStub)
-  return { sut, hasherStub, addAccountRepositoryStub, encrypterStub }
+  const sut = new DbAddAccount(hasherStub, addAccountRepositoryStub)
+  return { sut, hasherStub, addAccountRepositoryStub }
 }
 
 describe('DbAddAccount Usecase', () => {
@@ -76,13 +65,6 @@ describe('DbAddAccount Usecase', () => {
     })
   })
 
-  test('Should call Encrypter with correct account id (integration test)', async () => {
-    const { sut, encrypterStub } = makeSut()
-    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
-    await sut.add(makeFakeAccountData())
-    expect(encryptSpy).toHaveBeenCalledWith('valid_id')
-  })
-
   test('Should throw if Hasher throws (integration test)', async () => {
     const { sut, hasherStub } = makeSut()
     jest.spyOn(hasherStub, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
@@ -93,13 +75,6 @@ describe('DbAddAccount Usecase', () => {
   test('Should throw if AddAccountRepository throws (integration test)', async () => {
     const { sut, addAccountRepositoryStub } = makeSut()
     jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-    const promise: Promise<AccountModel> = sut.add(makeFakeAccountData())
-    await expect(promise).rejects.toThrow()
-  })
-
-  test('Should throw if Encrypter throws (integration test)', async () => {
-    const { sut, encrypterStub } = makeSut()
-    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise: Promise<AccountModel> = sut.add(makeFakeAccountData())
     await expect(promise).rejects.toThrow()
   })
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -5,16 +5,11 @@ export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
-    private readonly tokenGenerator: Encrypter
   ) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassord = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassord })
-    const accessToken = await this.tokenGenerator.encrypt(account.id)
-
-
-    return
-     account
+    return account
   }
 }
