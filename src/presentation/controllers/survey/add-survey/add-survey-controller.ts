265d8e4ee7680ce6add9d3fa5e33b7980af73f5c
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-12 13:56:55 -0300

    feat: ensure AddSurveyController calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Validation, HttpRequest, HttpResponse } from './add-survey-protocols'
+
+export class AddSurveyController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
+    return null
+  }
+}
--- src/validation/validators/email-validation.ts
+++ src/validation/validators/email-validation.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from '../../presentation/errors'
 import { Validation } from '../../presentation/protocols'
-import { EmailValidator } from '../../presentation/protocols/email-validator'
+import { EmailValidator } from '../protocols/email-validator'
 
 export class EmailValidation implements Validation {
   constructor (
