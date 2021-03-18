001ffa4ad0b5c23a97a44249db7467f93391795d
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-18 14:03:17 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -79,6 +79,23 @@ describe('Account Mongo Repository', () => {
       expect(accountData.password).toBe('any_password')
     })
 
+    test('Should return an account on loadByToken with role', async () => {
+      const { sut } = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const accountData = await sut.loadByToken('any_token', 'any_role')
+      expect(accountData).toBeTruthy()
+      expect(accountData.id).toBeTruthy()
+      expect(accountData.name).toBe('any_name')
+      expect(accountData.email).toBe('any_email@mail.com')
+      expect(accountData.password).toBe('any_password')
+    })
+
     test('Should return null if loadByEmail fails', async () => {
       const { sut } = makeSut()
       const accountData = await sut.loadByEmail('any_email@mail.com')
