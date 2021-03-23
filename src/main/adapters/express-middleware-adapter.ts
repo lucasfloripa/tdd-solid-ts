fcf20eec2db3aba56914642202d81a4be4e79b3e
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-23 14:58:49 -0300

    feat: ensure AddSurveyRoute returns 403 if no accessToken is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,17 @@
+import { Response, Request, NextFunction } from 'express'
+import { HttpRequest, HttpResponse, Middleware } from '../../presentation/protocols'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse: HttpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
+    }
+  }
+}
---
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -0,0 +1,11 @@
+import { DbLoadAccountByToken } from '../../../../../data/usecases/load-account-by-token/db-load-account-by-token'
+import { LoadAccountByToken } from '../../../../../domain/usescases/load-account-by-token'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../../config/env'
+
+export const makeDbLoadAccountbyToken = (): LoadAccountByToken => {
+  const decrypter = new JwtAdapter(env.jwtSecret)
+  const loadAccountByTokenRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(decrypter, loadAccountByTokenRepository)
+}
