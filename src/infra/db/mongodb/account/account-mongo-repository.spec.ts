139b83b03b64826893d60eb496fca0e93a08d9ec
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-23 15:51:58 -0300

    fix: ensure AccountMongoRepository allows admin to access any route

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -79,16 +79,16 @@ describe('Account Mongo Repository', () => {
       expect(accountData.password).toBe('any_password')
     })
 
-    test('Should return an account on loadByToken with role', async () => {
+    test('Should return an account on loadByToken with admin role', async () => {
       const { sut } = makeSut()
       await accountCollection.insertOne({
         name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         accessToken: 'any_token',
-        role: 'any_role'
+        role: 'admin'
       })
-      const accountData = await sut.loadByToken('any_token', 'any_role')
+      const accountData = await sut.loadByToken('any_token', 'admin')
       expect(accountData).toBeTruthy()
       expect(accountData.id).toBeTruthy()
       expect(accountData.name).toBe('any_name')
@@ -96,16 +96,39 @@ describe('Account Mongo Repository', () => {
       expect(accountData.password).toBe('any_password')
     })
 
-    test('Should return null if loadByEmail fails', async () => {
+    test('Should return null on loadByToken with invalid role', async () => {
       const { sut } = makeSut()
-      const accountData = await sut.loadByEmail('any_email@mail.com')
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const accountData = await sut.loadByToken('any_token', 'admin')
       expect(accountData).toBeFalsy()
     })
 
     test('Should return null if loadByToken fails', async () => {
       const { sut } = makeSut()
-      const accountData = await sut.loadByEmail('any_token')
+      const accountData = await sut.loadByToken('any_token')
       expect(accountData).toBeFalsy()
+    })
+
+    test('Should return an account on loadByToken if user is admin', async () => {
+      const { sut } = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'admin'
+      })
+      const accountData = await sut.loadByToken('any_token')
+      expect(accountData).toBeTruthy()
+      expect(accountData.id).toBeTruthy()
+      expect(accountData.name).toBe('any_name')
+      expect(accountData.email).toBe('any_email@mail.com')
+      expect(accountData.password).toBe('any_password')
     })
   })
 
