645eea48af845bd243b7b9b35e5fd2e63dbe7af1
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-03-26 16:01:05 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -22,17 +22,46 @@ describe('Account Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  test('Should add an survey on success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [
-        { image: 'any_image', answer: 'any_answer' },
-        { answer: 'other_answer' }
-      ],
-      date: new Date()
+  describe('add()', () => {
+    test('Should add an survey on success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [
+          { image: 'any_image', answer: 'any_answer' },
+          { answer: 'other_answer' }
+        ],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      const sut = makeSut()
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            { image: 'any_image', answer: 'any_answer' },
+            { answer: 'other_answer' }
+          ],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [
+            { image: 'other_image', answer: 'other_answer' },
+            { answer: 'other_answer' }
+          ],
+          date: new Date()
+        }
+      ])
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+    })
   })
 })
