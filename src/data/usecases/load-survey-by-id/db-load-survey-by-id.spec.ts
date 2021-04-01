46d0eeacbf99d13b7cc1b79c5137d17c9d7101fe
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-01 17:57:56 -0300

    test: ensure DbLoadSurveyById returns a survey on success

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -49,4 +49,10 @@ describe('DbLoadSurveyById', () => {
     await sut.loadById('any_id')
     expect(loadByidSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should return a survey on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.loadById('any_id')
+    expect(surveys).toEqual(makeFakeSurvey())
+  })
 })
