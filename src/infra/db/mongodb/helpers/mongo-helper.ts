3645af449b00ac49b58b56e6a8aaf5607c62c9b3
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-05 15:15:34 -0300

    fix: ensure SurveyMongoRepository uses MongoHelper.map in all methods

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -23,5 +23,8 @@ export const MongoHelper = {
   map (data: any): any {
     const { _id, ...rest } = data
     return { ...rest, id: _id }
+  },
+  mapCollection (collection: any[]): any[] {
+    return collection.map(c => MongoHelper.map(c))
   }
 }
--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -61,6 +61,7 @@ describe('Account Mongo Repository', () => {
       ])
       const surveys = await sut.loadAll()
       expect(surveys.length).toBe(2)
+      expect(surveys[0].id).toBeTruthy()
       expect(surveys[0].question).toBe('any_question')
     })
 
@@ -86,6 +87,7 @@ describe('Account Mongo Repository', () => {
       )
       const survey = await sut.loadById(res.ops[0]._id)
       expect(survey).toBeTruthy()
+      expect(survey.id).toBeTruthy()
     })
   })
 })
