e5d10005f5d9418acbbd0729b8f85d737da8a73f
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-06 09:47:50 -0300

    refactor: organize project files

--- src/main/factories/usecases/account/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/account/add-account/db-add-account-factory.ts
@@ -1,4 +1,4 @@
-import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
+import { DbAddAccount } from '@/data/usecases/account/add-account/db-add-account'
 import { AddAccount } from '@/domain/usescases/add-account'
 import { BcrypterAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
